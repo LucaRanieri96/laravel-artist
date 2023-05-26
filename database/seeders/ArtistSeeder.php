09f6bbfd43daee602e3ddfc3cbaa43d81257b081
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-26 17:43:51 +0200

    add seeder elements

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Artist;
 
 class ArtistSeeder extends Seeder
 {
@@ -12,8 +14,38 @@ class ArtistSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) { 
+            $artist = new Artist();
+
+            $artist->artista = $faker->randomElement([
+            'Andrea Bocelli',
+            'Ed Sheeran',
+            'Laura Pausini',
+            'Tiziano Ferro',
+            'Tina Turner',
+            'Zucchero',
+            'Beatols',
+            'Giorgia',
+            'Ligabue',
+            'Negramaro',
+            'Bob Marley'
+            ]);
+
+            $artist->nazionalita = $faker->randomElement([
+                'Italiana',
+                'Irlandese',
+                'Giamaicana',
+            ]);
+
+            $artist->prossimo_concerto = $faker->date();
+
+            $artist->ultimo_album = $faker->word();
+            
+            $artist->ultimo_singolo = $faker->word();
+
+            $artist->save();
+        }
     }
 }
