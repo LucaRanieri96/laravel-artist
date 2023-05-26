ed1dd0549956a025189907583cb8786333010483
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-26 18:57:31 +0200

    Add forelse to print artists

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,7 +17,7 @@
 
 <body>
     @include('partials.header')
-    <main class="bg-light">
+    <main class="bg-dark">
         @yield('content')
     </main>
     @include('partials.footer')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,34 @@
 @extends('layout.app')
 @section('content')
-    <h1>Artists</h1>
+
+
+    <h1 class="text-center">Artists</h1>
+    <div class="container">
+        <table class="table table-striped-columns table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">artista</th>
+                    <th scope="col">nazionalita</th>
+                    <th scope="col">prossimo_concerto</th>
+                    <th scope="col">ultimo_album</th>
+                    <th scope="col">ultimo_singolo</th>
+                </tr>
+            </thead>
+            <tbody>
+            @forelse ($artists as $artist)
+              <tr>
+                <td>{{$artist->artista}}</td>
+                <td>{{$artist->nazionalita}}</td>
+                <td>{{$artist->prossimo_concerto}}</td>
+                <td>{{$artist->ultimo_album}}</td>
+                <td>{{$artist->ultimo_singolo}}</td>
+            </tr>
+            @empty
+            <div class="col">
+            <p>Come back later!</p>
+            </div>
+            @endforelse
+            </tbody>
+          </table>
+    </div>
 @endsection
