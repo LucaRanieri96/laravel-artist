fdc74ff5d75d4ee1e3efd198c5458e19f5d20afd
DamianoCasolari <damianocasolari@gmail.com>
2023-05-27 12:17:31 +0200

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,12 @@
 @extends('layout.app')
 @section('content')
-
-
     <h1 class="text-center text-light mb-5">Artists</h1>
     <div class="container">
         <table class="table table-striped-columns table-dark">
+
+            <div class="text-light">
+                ciao
+            </div>
             <thead>
                 <tr>
                     <th scope="col">artista</th>
@@ -15,20 +17,20 @@
                 </tr>
             </thead>
             <tbody>
-            @forelse ($artists as $artist)
-              <tr>
-                <td>{{$artist->artista}}</td>
-                <td>{{$artist->nazionalita}}</td>
-                <td>{{$artist->prossimo_concerto}}</td>
-                <td>{{$artist->ultimo_album}}</td>
-                <td>{{$artist->ultimo_singolo}}</td>
-            </tr>
-            @empty
-            <div class="col">
-            <p>Come back later!</p>
-            </div>
-            @endforelse
+                @forelse ($artists as $artist)
+                    <tr>
+                        <td>{{ $artist->artista }}</td>
+                        <td>{{ $artist->nazionalita }}</td>
+                        <td>{{ $artist->prossimo_concerto }}</td>
+                        <td>{{ $artist->ultimo_album }}</td>
+                        <td>{{ $artist->ultimo_singolo }}</td>
+                    </tr>
+                @empty
+                    <div class="col">
+                        <p>Come back later!</p>
+                    </div>
+                @endforelse
             </tbody>
-          </table>
+        </table>
     </div>
 @endsection
