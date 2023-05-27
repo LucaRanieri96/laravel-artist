e915610e60b12932bfe0a59ef8b3f5f437aea445
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-27 02:34:04 +0200

    Add footer and Header

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,44 @@
-<footer>
-
-    <div>
-        Copyright
+<footer class="bg-dark text-white py-5">
+    <div class="container">
+        <div class="row row-cols-1 row-cols-lg-3">
+            <div class="col">
+                <h3>Quick Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                </ul>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+            <div class="col">
+                <h3>More Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                </ul>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+            <div class="col">
+                <h3>Legal Links</h3>
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Lorem.</a></li>
+                </ul>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row row-cols-1 row-cols-lg-3 -->
     </div>
+    <!-- /.container -->
 </footer>
